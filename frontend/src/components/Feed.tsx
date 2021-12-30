71a7d1d26f5eea6b8c0d1fe6774ee87c5abcaebd
alexwichti <alexwichti@gmail.com>
2021-12-30 12:21:58 +0100

    CSS refinements

--- frontend/src/components/Feed.tsx
+++ frontend/src/components/Feed.tsx
@@ -5,15 +5,15 @@ function Feed(props: { feed: LightSyndicationFeed }) {
 
 
     return (
-        <div className="my-3 mx-1 p-3 border dark:border-stone-700 rounded grow min-w-[15em] max-w-xl text-sm">
+        <div className="my-3 mx-1 p-3 border dark:border-stone-700 rounded grow  max-w-2xl text-sm truncate">
             <strong className='dark:text-stone-400'>{ props.feed.siteName }</strong>
             <ul>
                 {
                     props.feed.feedItems.map((f, i) => (
-                        <li key={ i }>
-                            <p className='my-0.5  max-w-xl flex'>
+                        <li key={ i } className=''>
+                            <p className='my-0.5  max-w-lg flex '>
                                 <em className='flex-none text-gray-400 dark:text-stone-600 w-12 inline-block'>{ getFormattedDate(f.publishDate) }</em>
-                                <a href={ f.url } className="truncate flex-grow" title={ f.summary } target="_blank" rel="noreferrer">{ f.title }</a>
+                                <a href={ f.url } className="truncate inline-block min-w-0" title={ f.summary } target="_blank" rel="noreferrer">{ f.title }</a>
                             </p>
                         </li>
                     ))
