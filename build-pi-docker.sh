8f8d0fd04313e12319b92be097ed306980d3656e
alexwichti <alexwichti@gmail.com>
2021-12-30 14:26:12 +0100

    dockerfile --rm

--- build-pi-docker.sh
+++ build-pi-docker.sh
@@ -1,5 +1,4 @@
 git pull 
 sudo docker stop ihp
-sudo docker container rm ihp
 sudo docker build -t ihp .
-sudo docker run -d --restart always -p 5000:5000 --name ihp ihp
+sudo docker run -d --restart always --rm -p 5000:5000 --name ihp ihp
